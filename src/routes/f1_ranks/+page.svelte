0d671607d4839ee94d4980e8b39a5fa0653433f1
Joseph JK VIT <115476354+JJKVIT@users.noreply.github.com>
2026-06-02 19:18:45 +0400

    style+more

--- src/routes/f1_ranks/+page.svelte
+++ src/routes/f1_ranks/+page.svelte
@@ -3,6 +3,7 @@
     import { read, utils } from "xlsx";
     import { base } from "$app/paths";
     import Badge from "$lib/components/badge.svelte";
+    import { scale } from "svelte/transition";
 
     const year = new Date().getFullYear();
     let allPagesData: Record<string, any[]> = $state({});
@@ -11,8 +12,9 @@
     let rows: any[] = $state([]);
     let loading = $state(true);
     let error = $state("");
-    let showOnlyAvg = $state(false);
     let showMode = $state(1);
+    let showAll = $state(true);
+    let rowData = $derived(showAll ? rows.slice(1) : rows.slice(1, 11));
 
     onMount(async () => {
         try {
@@ -41,10 +43,12 @@
             allPagesData = extractedData;
             rows = allPagesData["Ratings"];
 
-            keys = Object.keys(rows[1]);
-            headers = Object.keys(rows[0]).filter(
-                (key) => !key.includes("__EMPTY"),
-            );
+            if (rows && rows.length > 0) {
+                keys = Object.keys(rows[1] || {});
+                headers = Object.keys(rows[0] || {}).filter(
+                    (key) => !key.includes("__EMPTY"),
+                );
+            }
             loading = false;
         } catch (err) {
             console.error("Error loading Excel file:", err);
@@ -52,86 +56,168 @@
             loading = false;
         }
     });
+
+    const teamColors: Record<string, string> = {
+        Mercedes: "#26F0CE",
+        McLaren: "#FC7F00",
+        Ferrari: "#E5002C",
+        "Red Bull": "#356FC3",
+        Alpine: "#00A0E6",
+        "Racing Bulls": "#608CFB",
+        Haas: "#DDE0E1",
+        Williams: "#1766D7",
+        Audi: "#FD2C00",
+        Cadillac: "#A7A7AA",
+        "Aston Martin": "#229870",
+    };
 </script>
 
-<h1>F1 {year} Ratings</h1>
+<h1 class="title">F1 {year} Ratings</h1>
 
 {#if loading}
     <p>Loading...</p>
 {:else if error}
     <p>{error}</p>
 {:else}
-    <button class="toggle-btn" onclick={() => (showOnlyAvg = !showOnlyAvg)}>
-        {showOnlyAvg ? "Show All Sessions" : "Show Only Averages"}
-    </button>
-    <div class="outer-wrapper">
+    <div class="inline">
+        <div class="selector" style="--active: {showMode}; --count: 3;">
+            <button
+                class:selected={showMode == 0}
+                onclick={() => {
+                    showMode = 0;
+                }}
+            >
+                All Sessions
+            </button>
+            <button
+                class:selected={showMode == 1}
+                onclick={() => {
+                    showMode = 1;
+                }}>Only Average</button
+            >
+            <button
+                class:selected={showMode == 2}
+                onclick={() => {
+                    showMode = 2;
+                }}>Only Overall</button
+            >
+        </div>
+
+        <div
+            class="selector top"
+            style="--active: {showAll ? 0 : 1}; --count: 2;"
+        >
+            <button
+                class:selected={showAll}
+                onclick={() => {
+                    showAll = true;
+                }}>All</button
+            >
+            <button
+                class:selected={!showAll}
+                onclick={() => {
+                    showAll = false;
+                }}>Top 10</button
+            >
+        </div>
+    </div>
+
+    <div
+        class="outer-wrapper"
+        transition:scale={{ duration: 500, start: 0.95 }}
+    >
         <div class="scroll-wrapper">
             <table>
                 <thead>
                     <tr>
                         <th rowspan="2" class="rank">RANK</th>
-                        <th rowspan="2 " class="number">NO</th>
+                        <th rowspan="2" class="number">NO</th>
                         <th rowspan="2" class="driver">DRIVER</th>
                         <th rowspan="2" class="team">TEAM</th>
                         <th rowspan="2" class="avg">AVG</th>
 
-                        {#each headers as header}
-                            {@const isSprint = rows[0][header] === "SQ"}
-                            <th
-                                colspan={showOnlyAvg ? 1 : isSprint ? 5 : 3}
-                                class="race-name"
-                            >
-                                {header}
-                            </th>
-                        {/each}
-                    </tr>
-
-                    <tr>
-                        {#if !showOnlyAvg}
+                        {#if !(showMode == 2)}
                             {#each headers as header}
                                 {@const isSprint = rows[0][header] === "SQ"}
-                                {#each isSprint ? ["SQ", "SR", "Q", "R", "AVG"] : ["Q", "R", "AVG"] as sub}
-                                    <th class="sub-header">{sub}</th>
-                                {/each}
+                                <th
+                                    colspan={showMode == 1
+                                        ? 1
+                                        : isSprint
+                                          ? 5
+                                          : 3}
+                                    class="race-name"
+                                >
+                                    {header}
+                                </th>
                             {/each}
                         {/if}
                     </tr>
+                    {#if !(showMode == 2)}
+                        <tr>
+                            {#if !(showMode == 1)}
+                                {#each headers as header}
+                                    {@const isSprint = rows[0][header] === "SQ"}
+                                    {#each isSprint ? ["SQ", "SR", "Q", "R", "AVG"] : ["Q", "R", "AVG"] as sub}
+                                        <th class="sub-header">{sub}</th>
+                                    {/each}
+                                {/each}
+                            {/if}
+                        </tr>
+                    {/if}
                 </thead>
-                <tbody>
-                    {#each rows.slice(1) as row, i}
+                <tbody transition:scale={{ duration: 600, start: 0.95 }}>
+                    {#each rowData as row, i (row["DRIVER"])}
                         {@const name = row["DRIVER"].split(" ")}
+                        {@const team = row["TEAM"]}
+
                         <tr>
                             <td class="rank">{i + 1}</td>
-                            <td class="number"
-                                ><div class="box">{row["NO"]}</div></td
-                            >
+                            <td class="number">
+                                <div
+                                    class="box"
+                                    style:background-color={teamColors[team]}
+                                >
+                                    {row["NO"]}
+                                </div>
+                            </td>
                             <td class="driver">{`${name[0][0]}.${name[1]}`}</td>
                             <td class="team">
-                                {row["TEAM"] === "Racing Bulls"
-                                    ? "VCARB"
-                                    : row["TEAM"] === "Aston Martin"
-                                      ? "AMR"
-                                      : row["TEAM"]}
+                                <div
+                                    class="team-box"
+                                    style:background-color={teamColors[team]}
+                                >
+                                    {team === "Racing Bulls"
+                                        ? "VCARB"
+                                        : team === "Aston Martin"
+                                          ? "AMR"
+                                          : team}
+                                </div>
                             </td>
-                            <td class="avg"
-                                ><Badge rating={row["AVG"].toFixed(2)} /></td
-                            >
+                            <td class="avg">
+                                <Badge
+                                    rating={typeof row["AVG"] === "number"
+                                        ? row["AVG"].toFixed(2)
+                                        : row["AVG"]}
+                                />
+                            </td>
+                            {#if !(showMode == 2)}
+                                {#each keys.slice(5) as key, idx}
+                                    {@const nxtKey = keys.slice(5)[idx + 1]}
+                                    {@const isAvg =
+                                        !nxtKey || !nxtKey.includes("__EMPTY")}
 
-                            {#each keys.slice(5) as key, idx}
-                                {@const nxtKey = keys.slice(5)[idx + 1]}
-                                {@const isAvg =
-                                    !nxtKey || !nxtKey.includes("__EMPTY")}
-
-                                {#if !showOnlyAvg || isAvg}
-                                    <td class="badge-cell">
-                                        <Badge
-                                            rating={!isNaN(row[key])
-                                                ? row[key].toFixed(2)
-                                                : row[key]}
-                                        ></Badge>
-                                    </td>
-                                {/if}
-                            {/each}
+                                    {#if !(showMode == 1) || isAvg}
+                                        <td class="badge-cell">
+                                            <Badge
+                                                rating={typeof row[key] ===
+                                                "number"
+                                                    ? row[key].toFixed(2)
+                                                    : row[key]}
+                                            />
+                                        </td>
+                                    {/if}
+                                {/each}
+                            {/if}
                         </tr>
                     {/each}
                 </tbody>
@@ -141,19 +227,82 @@
 {/if}
 
 <style>
+    .inline {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        margin-bottom: 15px;
+    }
+    .selector {
+        background-color: color-mix(in srgb, white 8%, transparent);
+        border: 1.5px solid color-mix(in srgb, white 20%, transparent);
+        display: grid;
+        grid-template-columns: repeat(var(--count), 1fr);
+        align-items: center;
+        padding: 4px;
+        border-radius: 0.5rem;
+        position: relative;
+        z-index: 0;
+    }
+    .selector::before {
+        content: "";
+        position: absolute;
+        top: 4px;
+        bottom: 4px;
+        left: 4px;
+
+        width: calc((100% - 8px) / var(--count));
+
+        background-color: color-mix(in srgb, white 15%, transparent);
+        border-radius: 0.3rem;
+
+        transition: transform 0.25s cubic-bezier(0.4, 0, 0.2, 1);
+        transform: translateX(calc(100% * var(--active)));
+        z-index: -1;
+    }
+    .selector > button {
+        background: transparent;
+        border: none;
+        font-family: inherit;
+        line-height: 1;
+        outline: none;
+        margin: 0;
+
+        flex: 1;
+        text-align: center;
+
+        padding: 6px 12px;
+        font-size: 0.9rem;
+        font-weight: 500;
+
+        color: color-mix(in srgb, white 60%, transparent);
+        border-radius: 0.3rem;
+        cursor: pointer;
+        transition: color 0.2s ease;
+    }
+    .selector > button:hover {
+        color: white;
+    }
+    .selector > button.selected {
+        color: white;
+        font-weight: 600;
+    }
+    .title {
+        align-self: center;
+        justify-self: center;
+    }
     table {
-        margin-bottom: 5px;
+        margin: 0 auto 5px auto;
         border-spacing: 0;
     }
     .outer-wrapper {
         overflow: visible;
     }
-
     .scroll-wrapper {
         overflow: auto;
         max-height: 81.5vh;
         width: 100%;
-        /* Adds a gap between the table and the scrollbars */
         padding-bottom: 4.9px;
         padding-right: 4.8px;
     }
@@ -164,18 +313,11 @@
         vertical-align: middle;
         background-color: #14141a;
     }
-
-    .box {
-        width: 2.4rem;
-        margin: 0 auto;
-    }
-
     thead {
         position: sticky;
         top: 0;
         z-index: 10;
     }
-
     .rank,
     .number,
     .driver,
@@ -183,7 +325,21 @@
     .avg {
         position: sticky;
     }
+    .box {
+        font-weight: 800;
+        border-radius: 0.25rem;
+        width: 1.5rem;
+        padding: 0.08rem 0.1rem 0.08rem 0;
 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        aspect-ratio: 1/1;
+        margin: 0 auto;
+        color: #14141a;
+        font-style: italic;
+    }
     .rank {
         left: 0;
         width: 45px;
@@ -204,6 +360,7 @@
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+        font-weight: 600;
     }
     .team {
         left: 215px;
@@ -217,7 +374,18 @@
         min-width: 60px;
         max-width: 60px;
     }
-
+    .team-box {
+        font-weight: 900;
+        border-radius: 0.25rem;
+        padding: 0.15rem 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 4.5rem;
+        margin: 0 auto;
+        color: #14141a;
+        font-size: 0.85rem;
+    }
     thead .rank,
     thead .number,
     thead .driver,
